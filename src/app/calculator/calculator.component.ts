bd3e63c033488492bf4489198b5ed985ba4737bc
Romanos Papanikolaou <romanos.papanikolaou@gmail.com>
2024-05-23 20:27:15 +0200

    typescript details

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -17,7 +17,7 @@ export class CalculatorComponent implements OnInit {
       this.pressNum(key);
     } else if (key === '.' || key === ',') {
       this.pressNum('.');
-    } else if (key === '+' || key === '-' || key === '*' || key === '/') {
+    } else if (['+', '-', '*', '/'].includes(key)) {
       this.pressOperator(key);
     } else if (key === 'Enter') {
       this.getAnswer();
@@ -38,66 +38,40 @@ export class CalculatorComponent implements OnInit {
     }
   }
 
-  isNumeric(str: any) {
+  isNumeric(str: any): boolean {
     if (typeof str != 'string') return false;
     return !isNaN(str as any) && !isNaN(parseFloat(str));
   }
 
   pressNum(num: string) {
-    if (num == '.') {
-      if (this.input != '') {
-        const lastNum = this.getLastOperand();
-        if (lastNum.lastIndexOf('.') >= 0) return;
-      }
-    }
-    if (num == '0') {
-      if (this.input == '') {
-        return;
-      }
-      const PrevKey = this.input[this.input.length - 1];
-      if (
-        PrevKey === '/' ||
-        PrevKey === '*' ||
-        PrevKey === '-' ||
-        PrevKey === '+'
-      ) {
-        return;
-      }
-    }
-    this.input = this.input + num;
+    if (num === '.' && this.getLastOperand()?.includes('.')) return;
+    if (
+      num === '0' &&
+      (this.input === '' || ['/', '*', '-', '+'].includes(this.input.slice(-1)))
+    )
+      return;
+    this.input += num;
     this.calcAnswer();
   }
 
-  getLastOperand() {
-    let pos: number;
-    pos = this.input.toString().lastIndexOf('+');
-    if (this.input.toString().lastIndexOf('-') > pos)
-      pos = this.input.lastIndexOf('-');
-    if (this.input.toString().lastIndexOf('*') > pos)
-      pos = this.input.lastIndexOf('*');
-    if (this.input.toString().lastIndexOf('/') > pos)
-      pos = this.input.lastIndexOf('/');
-    return this.input.substr(pos + 1);
+  getLastOperand(): string | undefined {
+    const operands = this.input.split(/[\+\-\*\/]/);
+    return operands.length > 0 ? operands.pop() : undefined;
   }
 
   pressOperator(op: string) {
-    const lastKey = this.input[this.input.length - 1];
-    if (
-      lastKey === '/' ||
-      lastKey === '*' ||
-      lastKey === '-' ||
-      lastKey === '+'
-    ) {
+    const lastChar = this.input.slice(-1);
+    if (['/', '*', '-', '+'].includes(lastChar)) {
       return;
     }
-
-    this.input = this.input + op;
+    this.input += op;
     this.calcAnswer();
   }
 
   clear() {
-    if (this.input != '') {
-      this.input = this.input.substr(0, this.input.length - 1);
+    if (this.input) {
+      this.input = this.input.slice(0, -1);
+      this.calcAnswer();
     }
   }
 
@@ -107,32 +81,17 @@ export class CalculatorComponent implements OnInit {
   }
 
   calcAnswer() {
-    let formula = this.input;
-
-    let lastKey = formula[formula.length - 1];
-
-    if (lastKey === '.') {
-      formula = formula.substr(0, formula.length - 1);
+    try {
+      this.result = eval(this.input) || '';
+    } catch {
+      this.result = '';
     }
-    lastKey = formula[formula.length - 1];
-
-    if (
-      lastKey === '/' ||
-      lastKey === '*' ||
-      lastKey === '-' ||
-      lastKey === '+' ||
-      lastKey === '.'
-    ) {
-      formula = formula.substr(0, formula.length - 1);
-    }
-
-    this.result = eval(formula);
   }
 
   getAnswer() {
     this.calcAnswer();
-    this.input = this.result;
-    if (this.input == '0') this.input = '';
+    this.input = this.result.toString();
+    if (this.input === '0') this.input = '';
   }
 
   constructor() {}
